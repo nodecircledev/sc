b3f4b017f89a983111d32954163296347c25c0d0
rarealton <altonwayne@gmail.com>
2018-08-15 23:33:22 -0500

    Update install.sh

--- PRTX/install.sh
+++ PRTX/install.sh
@@ -271,9 +271,9 @@ function setup_node2(){
 }
 
 function setup_check(){
-  cat << EOF > $CONFIGFOLDER/$RUN_FILE
-  printex-cli -daemon -conf=/root/$CONFIGFOLDER/$CONFIG_FILE -datadir=/root/$CONFIGFOLDER getinfo
-  EOF
+cat << EOF > $CONFIGFOLDER/$RUN_FILE
+printex-cli -daemon -conf=/root/$CONFIGFOLDER/$CONFIG_FILE -datadir=/root/$CONFIGFOLDER getinfo
+EOF
 }
 
 function node_count(){
